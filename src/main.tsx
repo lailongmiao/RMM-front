43421ffd07c6ef772f72caf334c2c32bf277feb2
lailongmiao <2969314242@qq.com>
2025-01-09 19:20:36 +0800

    add the first sidebar and simplify the project file structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { SidebarProvider, SidebarTrigger } from './components/ui/sidebar.tsx'
+import { AppSidebar } from './components/app-sidebar.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+   <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+      </main>
+    </SidebarProvider>
   </StrictMode>,
 )
